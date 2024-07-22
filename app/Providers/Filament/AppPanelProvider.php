11aef65b7a1d0f913ceaec5feab9a0bca1ff3c93
Mo Khosh <mskhoshnazar@gmail.com>
2024-07-22 17:13:43 +0330

    change panel color

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -31,7 +31,7 @@ class AppPanelProvider extends PanelProvider
             ->login()
             ->registration()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Purple,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
