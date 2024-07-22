5897749c88cdc819983674f2e0e06d3706754303
Mo Khosh <mskhoshnazar@gmail.com>
2024-07-22 17:24:06 +0330

    add default attribute to projects

--- database/migrations/2024_07_22_101547_create_projects_table.php
+++ database/migrations/2024_07_22_101547_create_projects_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('notes')->nullable();
+            $table->boolean('default')->default(false);
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
