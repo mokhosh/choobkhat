033ccddf9ee3f93f0b02b1cdce03a8dc9f2bb829
Mo Khosh <mskhoshnazar@gmail.com>
2024-07-22 17:34:42 +0330

    update project policy

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -7,12 +7,9 @@ use App\Models\User;
 
 class ProjectPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
     public function viewAny(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -23,43 +20,18 @@ class ProjectPolicy
         //
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
     public function create(User $user): bool
     {
-        //
+        return true;
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
     public function update(User $user, Project $project): bool
     {
-        //
+        return $user->getKey() === $project->user_id;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
     public function delete(User $user, Project $project): bool
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Project $project): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Project $project): bool
-    {
-        //
+        return $user->getKey() === $project->user_id;
     }
 }
