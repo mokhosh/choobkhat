419c61ce7f316b56e3a166472fb398e9d4920451
Mo Khosh <mskhoshnazar@gmail.com>
2024-09-28 18:30:44 +0330

    use start and end date filters

--- app/Filament/Widgets/SessionOverview.php
+++ app/Filament/Widgets/SessionOverview.php
@@ -4,12 +4,13 @@ namespace App\Filament\Widgets;
 
 use App\Models\Project;
 use App\Models\Session;
-use Ariaieboy\Jalali\Jalali;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
+use Morilog\Jalali\Jalalian;
 
 class SessionOverview extends BaseWidget
 {
@@ -29,17 +30,25 @@ class SessionOverview extends BaseWidget
     public function getStats(): array
     {
         $project = $this->filters['project'] ? Project::find($this->filters['project']) : null;
+        $start = $this->filters['start'] ? Carbon::parse($this->filters['start']) : null;
+        $end = $this->filters['end'] ? Carbon::parse($this->filters['end']) : null;
 
-        $label = sprintf(
+        $label = (is_null($start) && is_null($end)) ? sprintf(
             'Working Hours Past %s %s',
-            $today = Jalali::now()->getDay(),
+            $today = Jalalian::now()->getDay(),
             Str::plural('Day', $today),
+        ) : sprintf(
+            'Working Hours since %s %s',
+            is_null($start) ? 'the start of this month' : Jalalian::fromCarbon($start)
+                ->format($start->year === now()->year ? 'n/j' : 'Y/n/j'),
+            is_null($end) ? '' : 'until '.Jalalian::fromCarbon($end)
+                ->format($end->year === now()->year ? 'n/j' : 'Y/n/j'),
         );
 
         return [
             Stat::make(
                 label: $label,
-                value: Session::getWorkingHoursSummary(project: $project),
+                value: Session::getWorkingHoursSummary($start, $end, $project),
             )->chart(
                 chart: Session::getWorkingHoursChart(project: $project),
             )->chartColor(
