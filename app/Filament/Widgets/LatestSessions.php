7462dd279c18bdce99a429699bb31d83ea5dbdba
Mo Khosh <mskhoshnazar@gmail.com>
2024-09-28 18:37:21 +0330

    use start and end date in table too

--- app/Filament/Widgets/LatestSessions.php
+++ app/Filament/Widgets/LatestSessions.php
@@ -9,6 +9,8 @@ use Filament\Tables\Actions;
 use Filament\Tables\Table;
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Support\Carbon;
+use Morilog\Jalali\Jalalian;
 
 class LatestSessions extends BaseWidget
 {
@@ -21,12 +23,16 @@ class LatestSessions extends BaseWidget
     public function table(Table $table): Table
     {
         $project = $this->filters['project'] ? Project::find($this->filters['project']) : null;
+        $start = $this->filters['start'] ? Carbon::parse($this->filters['start']) : Jalalian::now()->getFirstDayOfMonth()->toCarbon()->startOfDay();
+        $end = $this->filters['end'] ? Carbon::parse($this->filters['end']) : now();
 
         return SessionResource::table($table)
             ->poll('1s')
             ->query(
                 auth()->user()->sessions()->getQuery()->take(5)
                     ->when($project, fn ($query) => $query->where('project_id', $project->getKey()))
+                    ->when($start, fn ($query) => $query->where('start', '>=', $start))
+                    ->when($end, fn ($query) => $query->where('end', '<=', $end))
             )
             ->paginated(false)
             ->headerActions([
