e27b34904f22c4c3c548e7613c923e18d2dfe7fa
Mo Khosh <mskhoshnazar@gmail.com>
2024-11-23 22:26:30 +0330

    update rector config

--- rector.php
+++ rector.php
@@ -7,8 +7,6 @@ use Rector\Config\RectorConfig;
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/public',
-        __DIR__.'/resources',
         __DIR__.'/routes',
         __DIR__.'/tests',
     ])
